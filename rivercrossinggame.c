15244a8d9328d8196b7938470be9fc368bd13d9b
nsagammad <nikko.gammad@gmail.com>
2023-11-25 16:32:53 +0800

    added initial choice for user

--- rivercrossinggame.c
+++ rivercrossinggame.c
@@ -1,8 +1,59 @@
+/* This program allows the user to play an instance of the River Crossing game.
+    Besides this, this program allows the user to find the Expected Duration of an
+    initial position, or find the probability of the game outcome of two 
+    initial positions.
+
+    Author: Nikko Gammad
+*/
+
 #include <stdlib.h>
 #include <stdio.h>
+#include <time.h>
+
+void displayTitle();
+
+int dice = 2;
+int faces = 6;
+int max_chips = 12;
+int max_docks = 12;
 
 int main() {
-    printf("Hello World!\n");
+    char choice1 = '3'; //choice between playing the game or doing the calculations.
 
+    srand(time(0));
+
+    displayTitle();
+
+    //initial input
+    //1: Play the game
+    //2: Computation and Analysis
+    printf("Type [1] to play the original game.\n");
+    printf("Type [2] for the computations and analyses.\n");
+    printf("Your choice: ");
+    scanf(" %c", &choice1);
+
+    //check for invalid input
+    while (choice1 != '1' && choice1 != '2') {
+        printf("Invalid input. Your choice: ");
+        scanf(" %c", &choice1);
+    }
+
+    //play the original game
+    if (choice1 == '1') {
+        system("clear");
+        displayTitle();
+    }
+    //computation and analyses
+    else {
+        system("clear");
+        displayTitle();
+        printf("Coming soon...\n");
+    }
     return 0;
+}
+
+void displayTitle() {
+    printf("-------------------\n");
+    printf("River Crossing Game\n");
+    printf("-------------------\n");
 }
