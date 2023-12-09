7ac313749b0c5561973400b40445a2551e476e06
nsagammad <nikko.gammad@gmail.com>
2023-12-09 18:03:31 +0800

    initial java files added

---
+++ java/Position.java
@@ -0,0 +1,18 @@
+public class Position {
+    private final int MAX_CHIPSDOCKS = 35;
+    private final String DOCK_STRING = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    private int[] chips;
+    private int[] docks;
+    private int count;
+
+    public int getCount() {
+        return count;
+    }
+
+    public String chipsToString() {
+        String chipsStr = "";
+
+        return chipsStr;
+    }
+}
